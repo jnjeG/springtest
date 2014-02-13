279750c4f2db9f149edb84dc947e272fb2f561ac
jnjeG <qchycjj@126.com>
2014-02-13 13:44:27 +0800

    吃饭功能！

--- src/main/java/com/thingnet/controller/HomeController.java
+++ src/main/java/com/thingnet/controller/HomeController.java
@@ -1,13 +1,39 @@
 package com.thingnet.controller;
 
+import java.util.Random;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 	@RequestMapping(value="index.do")
 	public void index_jsp(Model model){
 		model.addAttribute("liming", "黎明你好");  
+	}
+	
+	/*@RequestMapping(value="/randomRes.do",method=RequestMethod.POST)
+	public void randomRes(@RequestParam(value = "name", required = false) String resNames[],Model model){
+		System.out.println(resNames);
+		model.addAttribute("result", "黎明你好");  
+	}*/
+	
+	
+	@RequestMapping(value="/randomRes.do",method=RequestMethod.POST)
+	public ModelAndView randomRes(HttpServletRequest request, HttpServletResponse response){
+		String names[] = request.getParameterValues("name");
+		int size  =names.length;
+		Random random = new Random();
+		int index = random.nextInt(size);
+		String result  = names[index];
+		request.setAttribute("result", result);
+		ModelAndView model = new ModelAndView("index");
+        return model;
 	}
 }
--- src/main/webapp/pages/index.jsp
+++ src/main/webapp/pages/index.jsp
@@ -1,7 +1,9 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>  
-
+<%
+	String context = request.getContextPath();
+%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -10,5 +12,22 @@
 </head>
 <body>
 <c:out value="${liming}"></c:out>
+<h2>不知道去哪吃饭好？没关系,天帮你!</h2>
+<form action="<%=context%>/randomRes.do" method="post">
+	<ul>
+		<li><input name="name" type="text" value="华辉" /></li>
+		<li><input name="name" type="text" value="麦当劳" /></li>
+		<li><input name="name" type="text" value="真功夫" /></li>
+		<li><input name="name" type="text" value="多营" /></li>
+		<li><input name="name" type="text" value="麦田鸡" /></li>
+		<li><input name="name" type="text" value="789" /></li>
+		<li><input name="name" type="text" value="烧鹅仔" /></li>
+		<li><input name="name" type="text" value="老刘家" /></li>
+	</ul>
+	<input type="submit" value="让天决定">
+</form>
+<h1>
+去...<c:out value="${result}"/>
+</h1>
 </body>
 </html>
